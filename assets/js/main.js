14ac388fcc57e9a9b2c4ffa4c6fe1cc434eb3ce3
alexhuynh <121414952+alexhuynh01@users.noreply.github.com>
2023-11-18 14:50:09 +0700

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 var videos = [
-    "/assets/video/LAMBORGHINI_URUS_16-9_H264_7sec.mp4",
-    "/assets/video/COBRA_SHELBY_16-9_NO_RATINGS_7sec.mp4",
-    "/assets/video/FORD_BRONCO_16-9_NO_RATINGS_7sec.mp4",
-    "/assets/video/B_ROLL_REVUELTO_16-9_H264_EMEA_7sec.mp4"
+    "assets/video/LAMBORGHINI_URUS_16-9_H264_7sec.mp4",
+    "assets/video/COBRA_SHELBY_16-9_NO_RATINGS_7sec.mp4",
+    "assets/video/FORD_BRONCO_16-9_NO_RATINGS_7sec.mp4",
+    "assets/video/B_ROLL_REVUELTO_16-9_H264_EMEA_7sec.mp4"
   ];
   var currentIndex = 0;
   
